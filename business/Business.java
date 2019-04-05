efba9a245eb7b4938acbf59083a08f4336c91937
Pizzarulle <48913785+Pizzarulle@users.noreply.github.com>
2019-04-05 12:03:35 +0200

    Add files via upload

--- business/Business.java
+++ business/Business.java
@@ -1,9 +1,12 @@
 package business;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import jdbc.JDBCConnector;
 
 /**
  * 
@@ -11,12 +14,40 @@ import java.util.List;
  *
  */
 public class Business {
-	static ResultSet rs = null;
+	private static ResultSet rs = null;
+	private static PreparedStatement stmt = null;
+	private static String storeId;
 	
-	public static List<Store> getAllStores() throws SQLException {
+	public Business(String storeId) {
+		Business.storeId = storeId;
+	}
+
+	/**
+	 * @return List of all active customer names associated with the storeId.
+	 */
+	public  List<String> getActiveCustomer() throws SQLException {
+		List<String> customers = new ArrayList<String>();
+
+		stmt = JDBCConnector.getInstance()
+				.createStatement("select distinct  " + "		concat(first_name, ' ', last_name) as name "
+						+ "from customer " + "where store_id = ? and active = '1'");
+
+		stmt.setString(1, storeId);
+		rs = stmt.executeQuery();
+
+		while (rs.next()) {
+			customers.add(rs.getString(1));
+		}
+		return customers;
+	}
+
+	/**
+	 * @return List of all stores.
+	 */
+	public List<Store> getAllStores() throws SQLException {
 		List<Store> stores = new ArrayList<Store>();
 
-		rs = JDBC.getStmt().executeQuery(
+		rs = JDBCConnector.getInstance().createStatement(
 
 				"select distinct " + "	store.store_id, " + // 1
 						"	address.address, " + // 2
@@ -28,7 +59,8 @@ public class Business {
 						+ "	inner join inventory on store.store_id = inventory.store_id "
 						+ "    inner join rental on inventory.inventory_id = rental.inventory_id "
 						+ "    inner join payment on rental.rental_id = payment.rental_id "
-						+ " group by store.store_id");
+						+ " group by store.store_id")
+				.executeQuery();
 
 		while (rs.next()) {
 			stores.add(new Store(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)));
@@ -36,10 +68,13 @@ public class Business {
 
 		return stores;
 	}
-	public static List<Store> getStore(String storeId) throws SQLException {
+	/**
+	 * @return List containing one store-object thats associated with the storeId.
+	 */
+	public  List<Store> getStoreInfo() throws SQLException {
 		List<Store> stores = new ArrayList<Store>();
 
-		rs = JDBC.getStmt().executeQuery(
+		stmt = JDBCConnector.getInstance().createStatement(
 
 				"select distinct " + "	store.store_id, " + // 1
 						"	address.address, " + // 2
@@ -51,8 +86,10 @@ public class Business {
 						+ "	inner join inventory on store.store_id = inventory.store_id "
 						+ "    inner join rental on inventory.inventory_id = rental.inventory_id "
 						+ "    inner join payment on rental.rental_id = payment.rental_id "
-						+ "where store.store_id = '"
-						+ storeId + "'");
+						+ "where store.store_id = ?");
+
+		stmt.setString(1, storeId);
+		rs = stmt.executeQuery();
 
 		while (rs.next()) {
 			stores.add(new Store(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4)));
@@ -60,20 +97,25 @@ public class Business {
 
 		return stores;
 	}
-
-	public static List<Staff> getStaff(String storeId) throws SQLException {
+	/**
+	 * @return List of all staff members associated with the storeId.
+	 */
+	public  List<Staff> getStaff() throws SQLException {
 		List<Staff> staffs = new ArrayList<Staff>();
 
+		stmt = JDBCConnector.getInstance().createStatement(
 
-		rs = JDBC.getStmt().executeQuery("select  " + "	store.store_id, " + // 1
-				"    sl.name, " + // 2
-				"    sl.address, " + // 3
-				"    sl.phone,  " + // 4
-				"    staff.email     " + // 5
-				"from staff_list as sl " + "	inner join store on sl.ID = store.manager_staff_id "
-				+ "    inner join staff on store.manager_staff_id = staff.staff_id "
-				+ "where store.store_id = '"
-				+ storeId + "'");
+				"select  " + "	store.store_id, " + // 1
+						"    sl.name, " + // 2
+						"    sl.address, " + // 3
+						"    sl.phone,  " + // 4
+						"    staff.email     " + // 5
+						"from staff_list as sl " + "	inner join store on sl.ID = store.manager_staff_id "
+						+ "    inner join staff on store.manager_staff_id = staff.staff_id "
+						+ "where store.store_id = ?");
+
+		stmt.setString(1, storeId);
+		rs = stmt.executeQuery();
 
 		while (rs.next()) {
 			staffs.add(new Staff(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
@@ -82,30 +124,35 @@ public class Business {
 		return staffs;
 	}
 
-	public static List<Payment> getStorePayments(String storeId) throws SQLException {
-
+	/**
+	 * @return List of all payments associated with the storeId.
+	 */
+	public  List<Payment> getStorePayments() throws SQLException {
 		List<Payment> payments = new ArrayList<>();
 
-			rs = JDBC.getStmt().executeQuery(
+		stmt = JDBCConnector.getInstance().createStatement(
 
-					"select" + "	payment_id, " + // 1
-							"	first_name, " + // 2
-							"	last_name, " + // 3
-							"	title, " + // 4
-							"	amount, " + // 5
-							"	payment_date, " + // 6
-							"	return_date " + // 7
-							"from payment " + "	inner join customer on payment.customer_id = customer.customer_id "
-							+ "	inner join rental on payment.rental_id = rental.rental_id "
-							+ "	inner join inventory on rental.inventory_id = inventory.inventory_id "
-							+ "	inner join film_text on inventory.film_id = film_text.film_id "
-							+ "where inventory.store_id = '" + storeId + "'");
+				"select" + "	payment_id, " + // 1
+						"	first_name, " + // 2
+						"	last_name, " + // 3
+						"	title, " + // 4
+						"	amount, " + // 5
+						"	payment_date, " + // 6
+						"	return_date " + // 7
+						"from payment " + "	inner join customer on payment.customer_id = customer.customer_id "
+						+ "	inner join rental on payment.rental_id = rental.rental_id "
+						+ "	inner join inventory on rental.inventory_id = inventory.inventory_id "
+						+ "	inner join film_text on inventory.film_id = film_text.film_id "
+						+ "where store.store_id = ?");
 
-			while (rs.next()) {
-				payments.add(new Payment(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
-						rs.getString(5), rs.getString(6), rs.getString(7)));
+		stmt.setString(1, storeId);
+		rs = stmt.executeQuery();
+		
+		while (rs.next()) {
+			payments.add(new Payment(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4),
+					rs.getString(5), rs.getString(6), rs.getString(7)));
 
-			}
+		}
 
 		return payments;
 
--- business/Main.java
+++ business/Main.java
@@ -11,21 +11,57 @@ import javafx.stage.Stage;
 public class Main {
 
 	public static void main(String[] args) throws SQLException {
-
-		for(Store s: Business.getAllStores()) {
+		
+//		Business Business = new Business();
+//
+//		for(Store s: Business.getAllStores()) {
+//			System.out.println(s);
+//		}
+//		
+//		System.out.println("-----");
+//		
+//		for(Store s: Business.getStore("1")) {
+//			System.out.println(s);
+//			for(Staff staff: Business.getStaff(s.getStore_id())) {
+//				System.out.println("     " + staff);
+//			}
+////			for(Payment p: Business.getStorePayments(s.getStore_id())) {
+////				System.out.println(p);
+////			}
+//			
+////			for(String customer: Business.getActiveCustomer("1")) {
+////				System.out.println(customer);
+////			}
+//		}
+		
+		
+		Business store_One = new Business("1");
+		
+		for(Store s: store_One.getAllStores()) {
 			System.out.println(s);
 		}
 		
 		System.out.println("-----");
+		System.out.println("STORE 1");
+		for(Store store : store_One.getStoreInfo()) {
+			System.out.println(store);
+		}
+		for(Staff staff : store_One.getStaff()) {
+			System.out.println("    " + staff);
+		}
 		
-		for(Store s: Business.getStore("1")) {
-			System.out.println(s);
-			for(Staff staff: Business.getStaff(s.getStore_id())) {
-				System.out.println("     " + staff);
-			}
-			for(Payment p: Business.getStorePayments(s.getStore_id())) {
-				System.out.println(p);
-			}
+		
+		
+	Business store_two = new Business("2");
+		
+	
+		System.out.println("-----");
+		System.out.println("STORE 2");
+		for(Store store : store_two.getStoreInfo()) {
+			System.out.println(store);
+		}
+		for(Staff staff : store_two.getStaff()) {
+			System.out.println("    " + staff);
 		}
 	}
 }
