f8b0775606eba9327a70af84172877a20a7fe27a
BouncingFrog <48982107+BouncingFrog@users.noreply.github.com>
2019-04-05 09:42:15 +0200

    Update JDBCConnector.java
    Updated with Singleton design pattern, a few new methods, and some descriptions for usage.
    New methods:
    closeConnections()
    connect(String dbConnection, String username, String password)
    createStatement(String sql)
    getInstance()
    getStatement()

--- JDBCConnector.java
+++ JDBCConnector.java
@@ -1,44 +1,126 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
+ * A connector class for JDBC that provides utility to create prepared statements.<br>
+ * An instance of the class is provided with getInstance().
  * 
  * @author Simon
  *
  */
 public class JDBCConnector {
 
-	private static String username = "";
-	private static String password = "";
-	private static String conn = "";
+	private static JDBCConnector instance;
+	private static Object lock = new Object();
 
-	public static Connection connect() throws SQLException {
-		return DriverManager.getConnection(conn, username, password);
+	private Connection connection = null;
+	private PreparedStatement statement = null;
+
+	private String dbConnection = "";
+	private String username = "";
+	private String password = "";
+
+	private JDBCConnector() {}
+
+	public static JDBCConnector getInstance() {
+		if (instance == null) {
+			synchronized (lock) {
+				if (instance == null) {
+					instance = new JDBCConnector();
+				}
+			}
+		}
+		return instance;
+	}
+
+	/**
+	 * @return Connection to database through the strings provided with the set methods in the JDBCConnector class.
+	 */
+	public Connection connect() {
+		if (connection == null) {
+			try {
+				connection = DriverManager.getConnection(dbConnection, username, password);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+
+	/**
+	 * 
+	 * @param dbConnection Address to your desired database.
+	 * @param username
+	 * @param password
+	 * @return Connection to database through the strings provided with the provided strings in the method.
+	 */
+	public Connection connect(String dbConnection, String username, String password) {
+		if (connection == null) {
+			try {
+				connection = DriverManager.getConnection(dbConnection, username, password);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return connection;
 	}
 	
-	public static String getUsername() {
+	/**
+	 * Input properly formatted SQL string. You do not need to explicitly get the Connection, 
+	 * but closeConnections() should be called after all usage is finished to prevent issues.
+	 * 
+	 * @param sql SQL formatted String
+	 * @return PreparedStatement with provided SQL String.
+	 */
+	public PreparedStatement createStatement(String sql) {
+		try {
+			statement = connect().prepareStatement(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return statement;
+	}
+	
+	/**
+	 * @return The current PreparedStatement without modifications.
+	 */
+	public PreparedStatement getStatement() {
+		return statement;
+	}
+
+	public void closeConnections() throws SQLException {
+		if (statement != null) {
+			statement.close();
+		}
+		if (connection != null) {
+			connection.close();
+		}
+	}
+	
+	public String getUsername() {
 		return username;
 	}
 
-	public static void setUsername(String username) {
-		JDBCConnector.username = username;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
-	public static String getPassword() {
+	public String getPassword() {
 		return password;
 	}
 
-	public static void setPassword(String password) {
-		JDBCConnector.password = password;
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
-	public static String getConnectionAddress() {
-		return conn;
+	public String getConnectionAddress() {
+		return dbConnection;
 	}
 
-	public static void setConnectionAddress(String conn) {
-		JDBCConnector.conn = conn;
+	public void setConnectionAddress(String dbConnection) {
+		this.dbConnection = dbConnection;
 	}
 
 }
